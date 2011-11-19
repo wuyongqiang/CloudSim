1e6d9e954d5af97c1cb149ec8fe9d59e1662d9d6
wuyongqiang <wuyongqiang@tsinghua.org.cn>
2011-11-19 18:14:22 +1000

    add  fixed time length support

--- CloudSimExamples/src/org/cloudbus/cloudsim/examples/power/SingleThreshold.java
+++ CloudSimExamples/src/org/cloudbus/cloudsim/examples/power/SingleThreshold.java
@@ -9,6 +9,8 @@ package org.cloudbus.cloudsim.examples.power;
  * Copyright (c) 2009, The University of Melbourne, Australia
  */
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,6 +20,7 @@ import java.util.Map.Entry;
 
 import org.cloudbus.cloudsim.Cloudlet;
 import org.cloudbus.cloudsim.CloudletSchedulerDynamicWorkload;
+import org.cloudbus.cloudsim.CloudletSchedulerDynamicWorkloadFixedTime;
 import org.cloudbus.cloudsim.DatacenterBroker;
 import org.cloudbus.cloudsim.DatacenterCharacteristics;
 import org.cloudbus.cloudsim.Log;
@@ -59,9 +62,11 @@ public class SingleThreshold {
 	 * Creates main() to run this example.
 	 *
 	 * @param args the args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		Log.setOutputFile("C:\\Users\\n7682905\\cloudsimoutput.txt");
 		Log.printLine("Starting SingleThreshold example...");
 
 		try {
@@ -176,6 +181,7 @@ public class SingleThreshold {
 			Cloudlet cloudlet = new Cloudlet(i, length, pesNumber, fileSize, outputSize, new UtilizationModelStochastic(), new UtilizationModelStochastic(), new UtilizationModelStochastic());
 			cloudlet.setUserId(brokerId);
 			cloudlet.setVmId(i);
+			cloudlet.setCloudletDuration(120); // 20 minutes
 			list.add(cloudlet);
 		}
 
@@ -202,7 +208,7 @@ public class SingleThreshold {
 
 		for (int i = 0; i < vmsNumber; i++) {
 			vms.add(
-				new Vm(i, brokerId, mips[i % mips.length], pesNumber, ram, bw, size, vmm, new CloudletSchedulerDynamicWorkload(mips[i % mips.length], pesNumber))
+				new Vm(i, brokerId, mips[i % mips.length], pesNumber, ram, bw, size, vmm, new CloudletSchedulerDynamicWorkloadFixedTime(mips[i % mips.length], pesNumber))
 			);
 		}
 
