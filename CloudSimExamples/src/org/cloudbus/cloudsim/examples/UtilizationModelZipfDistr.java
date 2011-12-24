819f69c1cc084747f48cabf09e84085530f382bf
Grant Wu <wuyongqiang@tsinghua.org.cn>
2011-12-24 11:34:00 +1000

    add probability distribution function ZipF support, however this may be not suitable for utilization distribution

---
+++ CloudSimExamples/src/org/cloudbus/cloudsim/examples/UtilizationModelZipfDistr.java
@@ -0,0 +1,23 @@
+package org.cloudbus.cloudsim.examples;
+
+import org.cloudbus.cloudsim.UtilizationModel;
+import org.cloudbus.cloudsim.distributions.UniformDistr;
+import org.cloudbus.cloudsim.distributions.ZipfDistr;
+
+public class UtilizationModelZipfDistr implements UtilizationModel {
+
+	transient private ZipfDistr zipf = null;
+	
+	public UtilizationModelZipfDistr() {
+		zipf = new ZipfDistr(0.5, 100); 
+	}
+	
+	/* (non-Javadoc)
+	 * @see cloudsim.power.UtilizationModel#getUtilization(double)
+	 */
+	@Override
+	public double getUtilization(double time) {
+		return zipf.sample() ;
+	}
+
+}
